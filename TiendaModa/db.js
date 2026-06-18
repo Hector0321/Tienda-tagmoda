e971c5163b09b066e2808c66650c36613bd31b33
Hector0321 <hectoralbertoarjonaalcocer@gmail.com>
2026-06-18 01:04:07 -0600

    Update db.js

--- TiendaModa/db.js
+++ TiendaModa/db.js
@@ -2,11 +2,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST || process.env.DB_HOST || 'localhost',
-  user: process.env.MYSQLUSER || process.env.DB_USER || 'root',
-  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD || '',
-  database: process.env.MYSQLDATABASE || process.env.DB_NAME || 'tienda_moda_node',
-  port: process.env.MYSQLPORT || process.env.DB_PORT || 3306,
+  uri: process.env.MYSQL_PUBLIC_URL || process.env.MYSQL_URL,
   waitForConnections: true,
   connectionLimit: 10,
   charset: 'utf8mb4'
