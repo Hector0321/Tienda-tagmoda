275c211a29a750bc863e0afd58693d155aa35159
Hector0321 <hectoralbertoarjonaalcocer@gmail.com>
2026-06-18 01:37:31 -0600

    Update db.js

--- TiendaModa/db.js
+++ TiendaModa/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST || process.env.DB_HOST || 'mysql.railway.internal',
+  host: process.env.MYSQLHOST || process.env.DB_HOST || 'thomas.proxy.rlwy.net',
   user: process.env.MYSQLUSER || process.env.DB_USER || 'root',
   password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD || 'arAGTtxGHHZetmpUCJRzkZeUHQPoZDUr',
   database: process.env.MYSQLDATABASE || process.env.DB_NAME || 'railway',
-  port: process.env.MYSQLPORT || process.env.DB_PORT || 3306,
+  port: process.env.MYSQLPORT || process.env.DB_PORT || 52358,
   waitForConnections: true,
   connectionLimit: 10,
   charset: 'utf8mb4'
